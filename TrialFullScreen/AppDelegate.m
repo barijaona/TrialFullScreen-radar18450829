b4955e1b0505ae91a48751aaa74fa9f6a3f45c51
Barijaona Ramaholimihaso <github.com@barijaona.com>
2014-08-31 21:55:40 +0300

    Load a test URL at the start of app

--- TrialFullScreen/AppDelegate.m
+++ TrialFullScreen/AppDelegate.m
@@ -12,7 +12,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-	// Insert code here to initialize your application
+	self.web.mainFrameURL = @"http://sindresorhus.com/screenfull.js/";
 }
 
 @end
